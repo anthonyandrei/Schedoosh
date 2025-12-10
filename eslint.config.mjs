078aef050b68ff3959bcb50350ec8e76187f53d0
CyberEzpertz <skyblaze24@gmail.com>
2025-12-10 23:56:12 +0800

    ci(lint): fix lint to not use flatConfig anymore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,27 @@
-import { FlatCompat } from "@eslint/eslintrc";
 import next from "eslint-config-next";
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-});
-
-const eslintConfig = [
+const eslintConfig = defineConfig([
   ...nextCoreWebVitals,
   ...nextTypescript,
   ...next,
-  ...compat.config({
-    extends: ["prettier"],
+  {
     rules: {
       "@typescript-eslint/no-empty-object-type": "off",
       "react-hooks/set-state-in-effect": "off",
       "@typescript-eslint/no-explicit-any": "off",
     },
-  }),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
   },
-];
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    "node_modules/**",
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
 
 export default eslintConfig;
